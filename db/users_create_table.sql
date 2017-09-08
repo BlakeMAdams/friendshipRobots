7ef8d51685b94cd90b8bf784d8f19c774cffc8e0
marachi2 <marissaf32@gmail.com>
2017-09-08 15:51:49 -0600

    Initial commit

---
+++ db/users_create_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    picture TEXT,
+    b_year INTEGER,
+    b_month TEXT,
+    b_day INTEGER,
+    eye_color TEXT,
+    hair_color TEXT,
+    hobby TEXT,
+    gender TEXT,
+    nada INTEGER,
+    first VARCHAR (80),
+    last VARCHAR (80)
+    );
